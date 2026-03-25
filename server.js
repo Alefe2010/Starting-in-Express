4e7e4dc1fa3c4f204d432c8db4bdb47985d112c6
Alefe2010 <amss34@aluno.ifal.edu.br>
2026-03-25 18:58:22 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const PORT = 3000
 
 let array = []
 
-
 app.use(express.json()) 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static("public"))
@@ -25,8 +24,17 @@ app.post("/usuario", (req, res) => {
 app.get("/usuarios", (req, res) => {
 
   let lista = array.map((user, index) => {
-    return `<p>${index + 1} - ${user[0]} (${user[1]})</p>`
-  }).join("")
+    return `<p>${index + 1} - ${user[0]} (${user[1]})  
+      <a href="/editar/${index}">
+          <button>Atualizar</button>
+        </a>
+      <a href="/deletar/${index}">
+        <button type="button">
+          Deletar
+        </button>
+        </a></p>
+       `
+  }).join(" ")
 
   res.send(`
     <h1>Lista de Usuários</h1>
@@ -35,6 +43,73 @@ app.get("/usuarios", (req, res) => {
     <a href="/">Voltar</a>
   `)
 })
+app.get("/editar/:id", (req, res) => {
+  const id = req.params.id
+  const usuario = array[id]
+
+  res.send(`
+    <h1>Editar Usuário</h1>
+    <form id="formEditar">
+      <input 
+        type="text" 
+        name="nome"
+        value="${usuario[0]}"
+      >
+      <input 
+        type="text"
+        name="gmail"
+        value="${usuario[1]}"
+      >
+      <button type="submit">
+        Atualizar
+      </button> 
+
+    </form>
+    <a href="/usuarios">Voltar</a>
+    <script>
+      document
+      .getElementById("formEditar")
+      .addEventListener("submit", async (e) => {
+        e.preventDefault()
+        const form = e.target
+
+        const nome = form.nome.value
+        const gmail = form.gmail.value
+
+        await fetch("/usuarios/${id}", {
+          method: "PATCH",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify({
+            nome,
+            gmail
+          })
+        })
+        window.location.href = "/usuarios"
+      })
+    </script>
+  `)
+})
+app.patch("/usuarios/:id", (req, res) => {
+  const id = req.params.id
+  const { nome, gmail } = req.body
+  if (nome) {
+    array[id][0] = nome
+  }
+  if (gmail) {
+    array[id][1] = gmail
+  }
+  res.json(array[id])
+})
+app.get("/deletar/:id", (req, res) => {
+  const id = req.params.id
+
+  console.log("Deletando:", array[id])
+  array.splice(id, 1)
+  res.redirect("/usuarios")
+})
+
 app.listen(PORT, () => {
   console.log("Servidor rodando em http://localhost:3000");
 })
